5fab4f25b7675a0889ff5d47e60c3b3c95452f6a
Rutuj Sudhindrakumar Shah <93345414+rutuj4@users.noreply.github.com>
2023-10-20 20:03:35 +0530

    Add files via upload

---
+++ OnlineBankingManagment/src/com/demo/bank/DBIntializer.java
@@ -0,0 +1,8 @@
+package com.demo.bank;
+
+public interface DBIntializer {
+	String DRIVER="com.mysql.cj.jdbc.Driver";
+	String CON_STRING="jdbc:mysql://127.0.0.1:3306/bank?";
+	String USERNAME="root";
+	String PASSWORD="@Rutuj@2002";
+}
